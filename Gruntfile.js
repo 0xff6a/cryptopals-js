95f75a3db50c8d046e1a3d5ca20d3fa61e9d12a4
Jeremy Fox <foxjerem@gmail.com>
2015-06-14 12:17:30 +0100

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+  
+  // Config
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: [ 'Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
+    },
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec',
+          quiet: false,
+          clearRequireCache: false
+        },
+        src: ['test/**/*.js']
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['<%= jshint.files %>'],
+        tasks: ['jshint', 'mochaTest'],
+        options: {
+          deboundeDelay: 10000
+        }
+      } 
+    }
+  });
+
+  // Plugins
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Register Tasks
+  grunt.registerTask('default', ['jshint', 'mochaTest']);
+
+};
---
+++ test/test.js
@@ -0,0 +1,7 @@
+var expect = require('expect.js');
+
+describe('Index', function() {
+  it('should log hello to the console', function() {
+    expect(true).to.be(true);
+  });
+});
